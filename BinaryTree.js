42686dd166c14b2309f8c3d2ab2fde5c91508b0b
Kauan Portela <kauanphbbb@gmail.com>
2021-06-16 22:18:15 -0300

    inserção com classes

--- BinaryTree.js
+++ BinaryTree.js
@@ -1,17 +1,47 @@
-const tree = {};
-
-function insert(tree, value) {
-  if (tree.root) {
-    if (value > tree.root) {
-      insert(tree.right, value);
-    } else if (value < tree.root) {
-      insert(tree.left, value);
-    }
-  } else {
-    tree.root = value;
-    tree.left = {};
-    tree.right = {};
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
   }
 }
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (node.value > newNode.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else if (node.value < newNode.value) {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
+
+const tree = new BinaryTree();
+
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
 
 console.log(tree);
